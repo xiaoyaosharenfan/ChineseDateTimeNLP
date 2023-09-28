41119960d7f99864990ee7a98a7a43033c690267
Alex <fanbinthu@163.com>
2023-09-29 02:03:16 +0800

    version1.0.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="ChineseDateTimeNLP",
-    version="1.0.0",
+    version="1.0.1",
     keywords=["nlp", "time nlp", "date nlp"],
     url="https://github.com/xiaoyaosharenfan/ChineseDateTimeNLP",
     author="Alex",
